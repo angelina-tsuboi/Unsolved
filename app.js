40778c6e099c422914ec938dd66a1c7add564960
angelina-tsuboi <angelina.t1832@gmail.com>
2020-02-15 18:02:10 -0800

    Added Google Authntication

--- app.js
+++ app.js
@@ -5,18 +5,6 @@ require("firebase/firestore");
 const port = 8549 || process.env.PORT;
 const app = express()
 
-var firebaseConfig = {
-    apiKey: "AIzaSyCruQFDCzLOn3BacxWenTP_qpdyVZqCksE",
-    authDomain: "unsolved-1c9fb.firebaseapp.com",
-    databaseURL: "https://unsolved-1c9fb.firebaseio.com",
-    projectId: "unsolved-1c9fb",
-    storageBucket: "unsolved-1c9fb.appspot.com",
-    messagingSenderId: "408884621201",
-    appId: "1:408884621201:web:faa334f3fcceb72c2ff430"
-};
-  
-firebase.initializeApp(firebaseConfig);
-
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
