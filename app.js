52a9a6cfd7fb48b729161e9325c50775e96a5f51
angelina-tsuboi <angelina.t1832@gmail.com>
2020-02-15 23:39:39 -0800

    Changed Styling for Blog Post

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
 const express = require('express')
+let path = require('path');
 var firebase = require("firebase/app");
 require("firebase/auth");
 require("firebase/firestore");
 const port = 8549 || process.env.PORT;
 const app = express()
+let ejs = require('ejs')
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    res.render('pages/index')
+})
+
+app.get('/malaysia', (req, res) => {
+    res.render('pages/malaysia')
+})
+
+app.get('/area51', (req, res) => {
+    res.render('pages/area51')
+})
+
+app.get('/jack', (req, res) => {
+    res.render('pages/jack')
+})
+
+app.get('/bigfoot', (req, res) => {
+    res.render('pages/bigfoot')
+})
+
+app.get('/alcatraz', (req, res) => {
+    res.render('pages/alcatraz')
+})
+
+app.get('/phoenix', (req, res) => {
+    res.render('pages/phoenix')
+})
+
+app.get('/bermuda', (req, res) => {
+    res.render('pages/bermuda')
 })
 
 app.listen(port, (err) => {
